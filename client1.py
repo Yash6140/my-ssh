8157597316eed696f6e4f5283045a4c661e6f82e
Sounava De <sounava7@outlook.com>
2026-05-23 17:07:13 +0530

    PHASE-3:RSA ENCRYPTION

--- client1.py
+++ client1.py
@@ -1,18 +1,26 @@
 
 import socket
 from crypto import encrypt, decrypt
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+import os
 
-#KEY = os.urandom(32)  
-KEY = bytes.fromhex('00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff')
-host='172.23.153.152' #public ip address of the server
+KEY = os.urandom(32)  
+#KEY = bytes.fromhex('00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff')
+host='172.17.61.95' #public ip address of the server
 port = 9090
 
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((host,port))
-MESSAGE = "Hi Server"
+pem_public_key = client.recv(2048)
+public_key = load_pem_public_key(pem_public_key)
+encrypted_key=public_key.encrypt(KEY, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+client.send(encrypted_key)
 
+MESSAGE = "Hi Server"
 client.send(encrypt(MESSAGE, KEY))
-response = client.recv(1024)
+response = client.recv(2048)
 #print(response)
 print(decrypt(response, KEY))
 
--- server1.py
+++ server1.py
@@ -1,10 +1,24 @@
 import socket
 from crypto import encrypt, decrypt
+from cryptography.hazmat.primitives.asymmetric import rsa 
+from cryptography.hazmat.primitives import serialization as serialisation
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+privatekey= rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key=privatekey.public_key()
+
+pem_public_key =public_key.public_bytes(
+  encoding=serialisation.Encoding.PEM,
+  format=serialisation.PublicFormat.SubjectPublicKeyInfo
+)
+
+
 
 #KEY = os.urandom(32) 
 KEY = bytes.fromhex('00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff') 
-host='172.23.153.152' #why l
-#host=socket.gethostbyname(socket.gethostname())
+#host='172.23.153.152'
+host=socket.gethostbyname(socket.gethostname())
 port = 9090
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -13,11 +27,15 @@ server.listen(5)
 while True:
     communication_socket, address = server.accept()
     print(f"Connected to {address}")
-    encrypted_message = communication_socket.recv(1024)
+    communication_socket.sendall(pem_public_key)
+    encrypted_key=communication_socket.recv(2048)
+    aes_gcm_key=privatekey.decrypt(encrypted_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+    print(f"Received encrypted key: {encrypted_key.hex()}")
+    encrypted_message = communication_socket.recv(2048)
     #print(f"Encrypt:{encrypted_message}")
-    message = decrypt(encrypted_message, KEY)
+    message = decrypt(encrypted_message, aes_gcm_key)
     print(f"The message is: {message}")
-    communication_socket.send(encrypt("Server has received the message", KEY))
+    communication_socket.send(encrypt("Server has received the message", aes_gcm_key))
     communication_socket.close()
     print("Connection closed")
 
